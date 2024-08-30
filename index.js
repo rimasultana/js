ddbb0f00811b588e295bd946f00043d1dbd1ca3a
rimasultana <rimasultana20225@gmail.com>
2024-08-30 06:15:55 +0600

    if else

---
+++ index.js
@@ -0,0 +1,26 @@
+// const age = 10;
+
+// // if (age > 10) {
+// //   console.log("apni dukta parben");
+// // } else{
+// //     console.log("sry, firta hobe apnka");
+// // }
+
+// if (age >= 15) {
+//   console.log("apni malai dukta parben");
+// } else if (age <= 13){
+//     console.log("apni kicu ride a jaita parben");
+// }
+// else{
+//     console.log("apni bacha der ride a dukta parben");
+// }
+// if(3 < 10 ){
+//     console.log("i have a smaller value");
+
+// }
+var x = 5;
+var y = 10;
+var devide = x/y;
+console.log(typeof devide);
+var value = devide.toFixed(4);
+console.log(typeof value);
