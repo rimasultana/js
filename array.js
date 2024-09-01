a4eba0a37a6947e1c92c53c1a7c69f05b6bfc126
rimasultana <rimasultana20225@gmail.com>
2024-09-01 20:49:34 +0600

    transmit

--- array.js
+++ array.js
@@ -8,9 +8,23 @@
 // numbers.push(33, 87);
 // console.log(numbers);
 
-const friends = ['bala', 'klam', 'kolam', 'ronal'];
-console.log(friends);
-friends.shift();
-console.log(friends);
-friends.unshift('pai;am');
-console.log(friends);
+// const friends = ['bala', 'klam', 'kolam', 'ronal'];
+// console.log(friends);
+// friends.shift();
+// console.log(friends);
+// friends.unshift('pai;am');
+// console.log(friends);
+
+// const friends = ['balam', 'kalam', 'kolam', 'rola,'];
+// console.log(friends.includes('gelam'));
+// console.log(friends.includes('khailam'));
+
+// if(friends.includes('balam')){
+//     console.log('party');
+// }else{
+//     console.log('no food, we are fasting');
+// }
+
+const friends =['balam', 'kalam', 'salam', 'rahman'];
+console.log(friends.indexOf('kalam'));
+console.log(friends.indexOf('tomato'));
