6f548c2474720b0dbb27048ab63bce0799827f51
rimasultana <rimasultana20225@gmail.com>
2024-09-01 09:54:10 +0600

    transmit

--- flot.js
+++ flot.js
@@ -1,3 +1,28 @@
-var x = 5;
-var y = 5;
-var devide = x / y;
+// var x = 5;
+// var y = 7;
+// var devide = x / y;
+// console.log(typeof devide);
+// var value = devide.toFixed(4);
+// console.log(typeof value);
+// // var value = parseInt(devide.toFixed(4));
+// var value = parseFloat(devide.toFixed(4));
+// console.log(typeof Number(value));
+
+// var x = 3;
+// console.log(x ** 4);
+
+// var y = 10;
+// console.log(10 % 10);
+// console.log(5%10);
+
+var isRaining = true;
+
+if (isRaining === true) {
+  console.log("will not go out");
+} else {
+  console.log("will go out side");
+}
+
+isRaining === true
+  ? console.log("will not go out")
+  : console.log("will go outside");
