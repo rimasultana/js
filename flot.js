64264b02ad70e521f26013ef802f3cf47caa5cf3
rimasultana <rimasultana20225@gmail.com>
2024-09-01 10:16:56 +0600

    transmit

--- flot.js
+++ flot.js
@@ -15,14 +15,49 @@
 // console.log(10 % 10);
 // console.log(5%10);
 
-var isRaining = true;
+// var isRaining = true;
+
+// if (isRaining === true) {
+//   console.log("will not go out");
+// } else {
+//   console.log("will go out side");
+// }
+
+// isRaining === true
+//   ? console.log("will not go out")
+//   : console.log("will go outside");
+
+// var money = 10;
+// var isRaining = true;
+// var texi = 15;
+
+// if (isRaining === true || money >= texi) {
+//   console.log("i will go to market");
+// }else{
+//     console.log("will not go market");
+// }
+
+// var money = 40;
+
+// if (money >= 100) {
+//   console.log("biriyani khabo");
+// } else if (money >= 50) {
+//   console.log("noodles khabo");
+// } else if (money >= 20) {
+//   console.log("singara khabo");
+// } else {
+//   console.log("bare fere jbo");
+// }
+
+var isRaining = false;
+var haveUmbrella = true;
 
 if (isRaining === true) {
-  console.log("will not go out");
+  if (haveUmbrella === true) {
+    console.log("will go out, i have a chata");
+  } else {
+    console.log("will not go to out side");
+  }
 } else {
-  console.log("will go out side");
+  console.log("sure, i go to outside");
 }
-
-isRaining === true
-  ? console.log("will not go out")
-  : console.log("will go outside");
