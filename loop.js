9b73c0e2ea33aecd29baee5cd209ed66793db0b2
rimasultana <rimasultana20225@gmail.com>
2024-09-02 10:57:50 +0600

    transmit

---
+++ loop.js
@@ -0,0 +1,16 @@
+const numbers = [1,45, 56, 67, 46, 46];
+
+console.log(numbers);
+for(const num of numbers){
+    console.log(num);
+
+}
+
+const fruit = ['banana', 'lichu', 'apple', 'orange', 'komola'];
+console.log(fruit);
+
+for(const fruits of fruit){
+    console.log(fruits);
+    console.log('i want eat');
+
+}
