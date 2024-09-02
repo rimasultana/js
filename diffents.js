fac2a3c7f563c55af496ff59a492b13675fc1c52
rimasultana <rimasultana20225@gmail.com>
2024-09-02 19:54:51 +0600

    for loop

---
+++ diffents.js
@@ -0,0 +1,13 @@
+// odd number
+
+for (let i = 0; i < 20; i++){
+    if(i%2===1){
+        console.log(i);
+    }
+}
+
+for (let i = 0; i < 20; i++){
+    if(i % 2 !== 0){
+        console.log(i);
+    }
+}
