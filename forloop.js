bcbcfeae8a7783d6c09fe83caaf5589dba085ce8
rimasultana <rimasultana20225@gmail.com>
2024-09-02 19:33:35 +0600

    for loop

---
+++ forloop.js
@@ -0,0 +1,45 @@
+// let num = 0;
+
+// while (num < 5){
+//     console.log(num);
+//     num++;
+// }
+
+// while and for loop er modda parthokko??????
+
+// for (let num = 0; num < 5; num++) {
+//     console.log(num);
+// }
+
+// for(let i = 0; i <= 10; i++){
+//     console.log(i);
+
+// }
+// Even
+// for (let i = 0 ; i <= 20; i= i+2){
+//     console.log(i);
+// }
+// // odd
+// for(let i = 1; i <= 20; i=i+2){
+// console.log(i);
+// }
+
+
+// 11 thake 20 porjoto shoonkka bar korbo
+
+
+
+// let sum = 0;
+// for (let i = 11; i<=20 ; i++){
+//     sum = sum + i;
+//     console.log(i);
+//     console.log(sum);
+// }
+
+// 0 thake 200 porjonto number addintion
+ let sum = 0
+for(let i = 0; i <=200; i++){
+    console.log(i);
+    sum = sum + i;
+    console.log(sum);
+}
---
+++ while.js
@@ -0,0 +1,40 @@
+// const number = 7;
+// if (number < 10) {
+//   console.log("choto number");
+// }
+// console.log("something new number");
+
+// let num = 0;
+
+// while (num < 5) {
+//   console.log("loping", num);
+//   // num = num + 1;
+//   // num+=1;
+//   num++;
+// }
+
+// let num = 1;
+// while (num <= 10) {
+//   console.log(num);
+//   num++;
+// }
+
+// let num = 1;
+// let sum = 0;
+
+// while (num <= 10) {
+//   console.log(num);
+//   sum = sum + num;
+//   console.log("sum:", sum);
+//   num++;
+// }
+// let nums = 1;
+
+// while (nums <= 10) {
+//   console.log(nums);
+
+
+//   if (nums % 2 === 0) {
+//     console.log("even number", nums)
+//   }
+// }
