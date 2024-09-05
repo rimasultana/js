2a7292b4c2a03706f7622960d68a9a07ef2d4ece
rimasultana <rimasultana20225@gmail.com>
2024-09-05 16:34:43 +0600

    task-1 solving

---
+++ brak.js
@@ -0,0 +1,27 @@
+// for (let i = 0; i < 15; i++){
+//     console.log(i);
+//     if(i >= 10){
+//         break;
+//     }
+// }
+// console.log('life is better without fellings');
+
+// let n = 54;
+// while(n > 25){
+//     console.log(n);
+//     if(n < 50){
+//         break;
+//     }
+//     n--;
+// }
+
+// i am done this loop, loop end
+
+// continue skip this one
+
+for (let i = 1; i < 10; i++){
+    if(i % 2 === 1){
+        continue;
+    }
+    console.log(i);
+}
---
+++ do-while.js
@@ -0,0 +1,14 @@
+
+// let n = 0;
+// do{
+//     console.log(n);
+//     n++;
+// }
+// while(n < 5);
+
+let n = 10;
+do{
+    console.log(n);
+    n++;
+}
+while(n < 5);
