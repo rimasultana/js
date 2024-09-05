7dd3dd06cd4d54926ff6d9647773fe49316af0f5
rimasultana <rimasultana20225@gmail.com>
2024-09-05 16:50:46 +0600

    task-3 solving

--- tast-1.js
+++ tast-1.js
@@ -40,4 +40,21 @@ var averageMark = totalMark / 5;
 console.log(averageMark);
 console.log(averageMark.toFixed(2));
 
+// Task-3
+// You task is to divide the given number by 5 and show the remainder as the output.
 
+// Input:
+// The first line of the input contains the number.
+
+// Output:
+// Print the remainder.
+
+// Sample Input:
+// 119
+
+// Sample Output:
+// 4
+
+var number = 119;
+var remainder =number % 5;
+console.log(remainder);
