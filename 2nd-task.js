22f678daf1ea7c2ca64198b6eb9ebf3b1dd52f91
rimasultana <rimasultana20225@gmail.com>
2024-09-05 19:33:56 +0600

    task-3 solving

---
+++ 2nd-task.js
@@ -0,0 +1,39 @@
+// Free Drinks
+//     - Burger more than 500tk: free Coke
+//     - Else Coke: 30tk
+// */
+
+const burgerPrize = 550;
+if (burgerPrize > 500) {
+  console.log("free Coke");
+} else {
+  console.log("coke pabe");
+}
+
+// BMI Calculator and Health Category
+
+// Create a JavaScript program that calculates the Body Mass Index (BMI) and assigns a
+//  health category based on the BMI value. Use nested if-else statements to determine the
+//   health category.
+
+//     - Calculate BMI using the formula: BMI = weight (kg) / (height (m))^2
+//     - BMI < 18.5, you are underweight.
+//     - BMI >= 18.5 and BMI <=24.9, you are normal.
+//     - BMI >=25 and BMI <= 29.9, you are overweight.
+//     - Otherwise, you are obese.
+
+// */
+
+// Grade Calculator
+
+// Create a simple JavaScript program that takes a student's score as input and returns their corresponding grade based on the following grading scale:
+
+// A: 90-100
+// B: 80-89
+// C: 70-79
+// D: 60-69
+// F: 0-59
+
+
+
+var 
