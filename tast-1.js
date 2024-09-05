d12e89709bd1e8b98c1e2dcf0de3fe66dbaaff8b
rimasultana <rimasultana20225@gmail.com>
2024-09-05 16:43:21 +0600

    task-2 solving

--- tast-1.js
+++ tast-1.js
@@ -13,3 +13,31 @@ var totalTk = 1000;
 var totalCost = 700;
 var cost = totalTk - totalCost;
 console.log(cost);
+
+// Task-2
+// Write a program to calculate the average marks of Mathematics, Biology, Chemistry, Physics, and Bangla of a student.
+
+// Input:
+// The first line of the input is the marks of the five subjects mentioned above, respectively.
+
+// Output:
+// Print the result in 2 decimal places.
+
+// Sample Input:
+// 75.25, 65, 80, 35.45, 99.50
+
+// Sample Output:
+// 71.04
+
+var mathMark = 75.25;
+var biologyMark = 65;
+var chimistryMark = 80;
+var physicsMark = 35.45;
+var banglaMark = 99.50;
+
+var totalMark = mathMark + biologyMark + chimistryMark + physicsMark + banglaMark;
+var averageMark = totalMark / 5;
+console.log(averageMark);
+console.log(averageMark.toFixed(2));
+
+
